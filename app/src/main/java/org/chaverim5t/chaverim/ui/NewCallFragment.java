587c673679f0979c98b56bc565195090f8dfa3d3
yakov <flyashi@gmail.com>
2015-08-19 17:55:08 -0400

    Rough New Call UI

--- app/src/main/java/org/chaverim5t/chaverim/ui/NewCallFragment.java
+++ app/src/main/java/org/chaverim5t/chaverim/ui/NewCallFragment.java
@@ -1,6 +1,5 @@
 package org.chaverim5t.chaverim.ui;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,23 +8,22 @@ import android.view.ViewGroup;
 
 import org.chaverim5t.chaverim.R;
 
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewCallFragment extends Fragment {
 
-
   public NewCallFragment() {
     // Required empty public constructor
   }
-
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_new_call, container, false);
+    View view = inflater.inflate(R.layout.fragment_new_call, container, false);
+
+    return view;
   }
 
 
